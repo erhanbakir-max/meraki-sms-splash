f11af446d9e4e369dc64281e7bed6e328735dc11
erhanbakir-max <erhanbakir@gmail.com>
2026-02-12 10:56:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+import { createClient } from 'redis';
+
+const redis = createClient({
+  url: process.env.REDIS_URL
+});
+
+redis.on('error', (err) => console.error('Redis Client Error', err));
+
+await redis.connect();
+
+console.log("Redis connected");
+
 import http from "http";
 import { URL } from "url";
 import { randomInt } from "crypto";
